f07cf806c9e4a14da1c18ec85dd7c21ae1c930ea
Zainab Mehdee <zainab.mehdee63@ed.cna.nl.ca>
2020-12-02 09:25:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+num_to_string = str(two_digit_number)
+
+
+
+
+
+
+
+
